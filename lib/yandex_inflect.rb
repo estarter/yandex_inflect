69a49e2e998f2b201c840b2229798806478ab760
estarter <estarter@gmail.com>
2011-06-16 02:01:30 +0200

    fixed KCODE definition for ruby1.9

--- lib/yandex_inflect.rb
+++ lib/yandex_inflect.rb
@@ -1,4 +1,5 @@
-$KCODE = 'u'
+# -*- encoding: utf-8 -*-
+$KCODE = 'u' if RUBY_VERSION < '1.9.0'
 
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
